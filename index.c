ad8af1724cb64c8ad228cea3e7b4b744e90dcff3
jadavnishith <mrnick076@gmail.com>
2023-12-25 16:37:22 +0530

    alphabet

---
+++ index.c
@@ -0,0 +1,35 @@
+
+//arithmetic operator
+
+#include <stdio.h>
+
+int main()
+{
+    //------A--------
+    printf("      *  \n");
+    printf("     * *\n");
+    printf("    *   *\n");
+    printf("   * * * *\n");
+    printf("  *       *\n");
+    printf(" *         *\n\n");
+    
+    //------B--------
+    printf(" * * * * *\n");
+    printf(" *        *\n");
+    printf(" *       *\n");
+    printf(" * * * *\n");
+    printf(" *       *\n");
+    printf(" *        *\n");
+    printf(" * * * * *\n");
+    
+    //------c--------
+    printf("      * *\n");
+    printf("        *\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    
+    
+    return 0;
+}
